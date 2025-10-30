3958b09853dab5be978f6f5d50e742b109a12a28
guilyan <act.gkezeiah09@gmail.com>
2025-10-30 13:14:32 +0800

    commit all

--- obj/Debug/net8.0/Irish_Beauty-Product.AssemblyInfo.cs
+++ obj/Debug/net8.0/Irish_Beauty-Product.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Irish_Beauty-Product")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83ca2860c8ea6d9b59c01bf760467a5c7e83692")]
 [assembly: System.Reflection.AssemblyProductAttribute("Irish_Beauty-Product")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Irish_Beauty-Product")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
